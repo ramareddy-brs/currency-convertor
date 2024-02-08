fb8a71ef919cce3ded5135b733be70459e1a32b7
Ramareddy <ramareddy9652@gmail.com>
2024-02-08 12:38:59 +0530

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+const currencyEl_one = document.getElementById('currency-one');
+const amountEl_one = document.getElementById('amount-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_two = document.getElementById('amount-two');
+
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+// Fetch exchange rates and update the DOM
+function caclulate() {
+    const currency_one = currencyEl_one.value;
+    const currency_two = currencyEl_two.value;
+    let amount_one = parseInt(amountEl_one.value);
+
+    fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data);
+            const rate = data.rates[currency_two];
+            amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+            let rate2 = amountEl_two.value
+
+            rateEl.innerText = `${amount_one} ${currency_one} = ${rate2} ${currency_two}`;
+
+    
+        });
+}
+
+// Event listeners
+currencyEl_one.addEventListener('change', caclulate);
+amountEl_one.addEventListener('input', caclulate);
+currencyEl_two.addEventListener('change', caclulate);
+amountEl_two.addEventListener('input', caclulate);
+
+swap.addEventListener('click', () => {
+    const temp = currencyEl_one.value;
+    currencyEl_one.value = currencyEl_two.value;
+    currencyEl_two.value = temp;
+    caclulate();
+});
+
+function setTheme(theme) {
+    document.documentElement.style.setProperty('--primary-color', theme);
+    localStorage.setItem('cc-theme', theme);
+}
+caclulate();
+setTheme(localStorage.getItem('cc-theme') || '#1A4B84');
